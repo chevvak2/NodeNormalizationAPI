01deb66104f4576c0f5a2ee81e3aa07efc71d37d
ctrl-schaff <schaffjohnathan@gmail.com>
2026-03-04 14:55:14 -0600

    Copy the original deploy directory from annotator

---
+++ deploy/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+projectName="biothings-annotator"
+namespace="bte"
+
+#set +x
+#export $(egrep -v '^#' .env)
+
+sed -i.bak \
+    -e "s/DOCKER_VERSION_VALUE/${BUILD_VERSION}/g" \
+    values.yaml
+rm values.yaml.bak
+
+helm -n ${namespace} upgrade --install ${projectName} -f values-ncats.yaml ./
